25fac3d7378c4069fb7e75dbf41ded808218e5f2
Dyamen1411 <massias.antoine@gmail.com>
2024-03-12 20:28:07 +0100

    Fix: cd now shows the correct error messages.

--- src/core/exec/builtins/builtin_cd.c
+++ src/core/exec/builtins/builtin_cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amassias <amassias@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amassias <amassias@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 13:13:49 by amassias          #+#    #+#             */
-/*   Updated: 2024/03/01 01:11:33 by amassias         ###   ########.fr       */
+/*   Updated: 2024/03/12 20:26:13 by amassias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,13 +63,12 @@ static t_ms_error	_update_pwd(
 t_ms_error	builtin_cd(
 				t_exec_ctx *ctx,
 				char **argv,
-				char **envp
+				char __attribute__((unused)) **envp
 				)
 {
 	size_t	count;
 	int		code;
 
-	(void)envp;
 	count = _count_args(argv);
 	if (count != 2)
 	{
@@ -80,14 +79,9 @@ t_ms_error	builtin_cd(
 			dprintf(STDERR_FILENO, ERROR_NOT_ENOUGH_ARGS "\n");
 		return (env_set_code(ctx->env_ctx, 1));
 	}
-	if (access(argv[1], F_OK) != 0)
-	{
-		dprintf(STDERR_FILENO, ERROR_NO_FILE "\n", argv[1]);
-		return (env_set_code(ctx->env_ctx, 1));
-	}
 	code = chdir(argv[1]) == -1;
 	if (code != 0)
-		dprintf(STDERR_FILENO, ERROR_CANT_CHANGE_DIR "\n", argv[1]);
+		(dprintf(STDERR_FILENO, "minishell: cd: "), perror(argv[1]));
 	if (_update_pwd(ctx))
 		return (env_set_code(ctx->env_ctx, 1), MS_FATAL);
 	return (env_set_code(ctx->env_ctx, code));
