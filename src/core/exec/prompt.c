9f65313dc4c84ed7ce00062f446d44ac42c404fa
Lailouezzz <alanlebouder@gmail.com>
2024-09-24 18:21:55 +0200

    Fix: username was not retrieved in a standard way Switched getlogin method to geteuid and getpwuid

--- src/core/exec/prompt.c
+++ src/core/exec/prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amassias <amassias@student.42lehavre.fr    +#+  +:+       +#+        */
+/*   By: ale-boud <ale-boud@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 08:11:11 by amassias          #+#    #+#             */
-/*   Updated: 2024/03/20 13:14:21 by amassias         ###   ########.fr       */
+/*   Updated: 2024/09/24 18:17:14 by ale-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@
 #include <unistd.h>
 #include <limits.h>
 #include <libft.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <readline/readline.h>
 
@@ -49,6 +50,14 @@
 #define PROMPT_FMT_L1 "\e[0;32m(\e[1;34m%s@%s\e[0;32m)=[\e[1;37m%s\e[0;32m]"
 #define PROMPT_FMT_L2 "\e[1;%dm$\e[0;0m "
 
+// ************************************************************************** //
+// *                                                                        * //
+// * Utils declaration                                                      * //
+// *                                                                        * //
+// ************************************************************************** //
+
+static const char	*_get_username(void);
+
 /* ************************************************************************** */
 /*                                                                            */
 /* Header implementation                                                      */
@@ -59,11 +68,11 @@ char	*ms_prompt(
 			t_exec_ctx *ctx
 			)
 {
-	static char	hostname[HOST_NAME_MAX + 1] = {0};
-	const char	*login = getlogin();
-	char		*cwd;
-	char		*result;
-	char		*line;
+	static char			hostname[HOST_NAME_MAX + 1] = {0};
+	const char			*username = _get_username();
+	char				*cwd;
+	char				*result;
+	char				*line;
 
 	if (gethostname(hostname, HOST_NAME_MAX))
 		return (NULL);
@@ -71,17 +80,33 @@ char	*ms_prompt(
 	if (cwd == NULL)
 		return (NULL);
 	result = malloc(1 + snprintf(NULL, 0, PROMPT_FMT_L1 "\n" PROMPT_FMT_L2,
-				login, hostname, cwd, 30));
+				username, hostname, cwd, 30));
 	if (result == NULL)
 		return (free(cwd), NULL);
 	if (ctx->env_ctx->current_code)
-		sprintf(result, PROMPT_FMT_L1 "\n" PROMPT_FMT_L2, login, hostname, cwd,
+		sprintf(result, PROMPT_FMT_L1 "\n" PROMPT_FMT_L2, username, hostname, cwd,
 			COLOR_CODE_RED);
 	else
-		sprintf(result, PROMPT_FMT_L1 "\n" PROMPT_FMT_L2, login, hostname, cwd,
+		sprintf(result, PROMPT_FMT_L1 "\n" PROMPT_FMT_L2, username, hostname, cwd,
 			COLOR_CODE_GREEN);
 	free(cwd);
 	line = readline(result);
 	free(result);
 	return (line);
+}
+
+// ************************************************************************** //
+// *                                                                        * //
+// * Utils implementation                                                   * //
+// *                                                                        * //
+// ************************************************************************** //
+
+static const char	*_get_username(void)
+{
+	const int			euid = geteuid();
+	const struct passwd	*pw = getpwuid(euid);
+
+	if (pw == NULL)
+		return (NULL);
+	return (pw->pw_name);
 }
